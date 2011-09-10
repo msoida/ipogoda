c09c5c968b83e2e0797c4b3574858c6a2cb473a3
Michal Soida <msoida@gmail.com>
2011-09-10 23:08:38 +0200

    Wstępne przygotowanie pod GPS

--- image.php
+++ image.php
@@ -6,11 +6,16 @@
 // http://creativecommons.org/licenses/by-nc-sa/3.0/pl/legalcode
 //
 if(array_key_exists("model",$_REQUEST)) $model = $_REQUEST["model"];
+else $model = "um";
+if(array_key_exists("lat",$_REQUEST)) $lat = $_REQUEST["lat"];
+else $lat = 0;
+if(array_key_exists("long",$_REQUEST)) $long = $_REQUEST["long"];
+else $long = 0;
 if(array_key_exists("hash",$_REQUEST)) $ifhash = true;
 else $ifhash = false;
 $host  = $_SERVER['HTTP_HOST'];
 $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-$extra = "makeimage.php?model={$model}";
+$extra = "makeimage.php?model={$model}&lat={$lat}&long={$long}";
 $adres = "http://$host$uri/$extra";
 $obrazek = file_get_contents($adres);
 $obr64 = "data:image/PNG;base64," . base64_encode($obrazek);
--- makeimage.php
+++ makeimage.php
@@ -5,63 +5,46 @@
 //     - Użycie niekomercyjne - Na tych samych warunkach 3.0 Polska
 // http://creativecommons.org/licenses/by-nc-sa/3.0/pl/legalcode
 //
-header("Content-Type: image/PNG");
 if(array_key_exists("model",$_REQUEST)) $model = $_REQUEST["model"];
+else $model = "um";
+if(array_key_exists("lat",$_REQUEST)) $lat = $_REQUEST["lat"];
+else $lat = 0;
+if(array_key_exists("long",$_REQUEST)) $long = $_REQUEST["long"];
+else $long = 0;
 
 
+//przygotowywanie adresu
+if ($lat!=0 || $long!=0)
+{
+exit("ERROR: Trying to change coordinates. Not possible in this version.");
+}
+else
+{
 if ($model == "coamps") $filename = "http://new.meteo.pl/metco/mgram_pict.php?ntype=2n&row=151&col=91&lang=pl";
 else $filename = "http://new.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=466&col=232&lang=pl";
+}
 
+//pobieranie obrazka
 $obr_orig = imagecreatefrompng($filename);
 $size = getimagesize($filename);
 $origw = $size[0];
 $origh = $size[1];
 
+
+//przerabianie obrazka
+//wymiary ekranu:
 //320 x 480
 if ($origw > 480)
 {
 $newh = 480*$origh/$origw;
 $obrazek = imagecreatetruecolor(480, $newh);
-//imagecopyresampled($obrazek, $obr_orig, 0, 0, 0, 29, 480, $newh, $origw, 280);
 imagecopyresampled($obrazek, $obr_orig, 0, 0, 0, 0, 480, $newh, $origw, $origh);
 }
 else
 {
 $obrazek = imagecreatetruecolor($origw, $origh);
-//imagecopyresized($obrazek, $obr_orig, 0, 0, 0, 29, $origw, 280, $origw, 280);
 imagecopyresized($obrazek, $obr_orig, 0, 0, 0, 0, $origw, $origh, $origw, $origh);
 }
-
-//imagetruecolortopalette($obrazek,0,256);
-
-//imagecolorset($obrazek, imagecolorclosest($obrazek,211,233,251), 255, 255, 255);
-//imagecolorset($obrazek, imagecolorclosest($obrazek,249,233,230), 255, 255, 255);
-//imagecolorset($obrazek, imagecolorclosest($obrazek,255,251,240), 255, 255, 255);
-//imagecolorset($obrazek, imagecolorclosest($obrazek,252,254,252), 255, 255, 255);
-//imagecolorset($obrazek, imagecolorclosest($obrazek,228,226,228), 255, 255, 255); //szary
-//imagecolorset($obrazek, imagecolorclosest($obrazek,220,222,220), 255, 255, 255);
-//imagecolorset($obrazek, imagecolorclosest($obrazek,132,190,228), 132, 206, 255); //ciemnoniebieski
-
-//imagecolorset($obrazek, imagecolorexact($obrazek,248,235,229), 255, 255, 255);
-
-
-//imagepalettetotruecolor($obrazek);
-//function imagepalettetotruecolor(&$img)
-//    {
-//        if (!imageistruecolor($img))
-//        {
-//            $w = imagesx($img);
-//            $h = imagesy($img);
-//            $img1 = imagecreatetruecolor($w,$h);
-//            imagecopy($img1,$img,0,0,0,0,$w,$h);
-//            $img = $img1;
-//        }
-//    }
-
-
-//imagecolortransparent($obrazek, imagecolorallocate($obrazek,255,255,255));
-
-
-
+header("Content-Type: image/PNG");
 imagepng($obrazek);
 ?>
